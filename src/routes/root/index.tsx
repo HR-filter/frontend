f0a6898b4a34dba12f728ea3ccc9d9f1a5e57d6d
Lvova Alina <lvova.aline@gmail.com>
2023-10-24 19:58:00 +0400

    fix: fix structure

--- src/routes/root/index.tsx
+++ src/routes/root/index.tsx
@@ -1,5 +1,4 @@
 import { Outlet, Link, useLocation } from 'react-router-dom';
-import TestButton from '../../components/ui-kit/test-button';
 
 function Root() {
   const location = useLocation();
@@ -9,7 +8,6 @@ function Root() {
       <h1>Шапка</h1>
       <main>
         <Outlet />
-        <TestButton />
       </main>
 
       {location.pathname === '/' ? (
