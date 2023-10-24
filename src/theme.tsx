24c53d604fc817a43c0c066293417e0bc86d0c66
Lvova Alina <lvova.aline@gmail.com>
2023-10-24 21:31:46 +0400

    feat: add disabled state to test button

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,7 +4,23 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#5A9BFF',
+      light: '#dde0e4', // Цвет для неактивного состояния. Подумать как можно назвать по-другому
+      main: '#5a9bff', // Основной синий цвет
+      dark: '#1d6bf3', // Темный синий для активных элементов
+    },
+    secondary: {
+      main: '#87cc9e', // Зеленый вторичный цвет
+    },
+    error: {
+      main: '#ff0200', // Цвет ошибки
+    },
+    background: {
+      default: '#ffffff', // Цвет фона по умолчанию
+      paper: '#f9fafb', // Цвет фона бумаги
+    },
+    text: {
+      primary: '#1a1b22', // Основной цвет текста
+      secondary: '#797981', // Вторичный цвет текста
     },
   },
 });
--- src/ui/test-button/index.tsx
+++ src/ui/test-button/index.tsx
@@ -1,5 +1,20 @@
 import Button from '@mui/material/Button';
 
 export default function TestButton() {
-  return <Button variant="contained">Тестовая кнопка из MUI</Button>;
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      sx={{
+        '&.Mui-disabled': {
+          cursor: 'not-allowed',
+          pointerEvents: 'all !important',
+          backgroundColor: (theme) => theme.palette.primary.light,
+        },
+      }}
+      disabled
+    >
+      Тестовая кнопка из MUI
+    </Button>
+  );
 }
