b702842309ba2782460cf472a317d11837dfc248
Lvova Alina <lvova.aline@gmail.com>
2023-10-30 21:58:58 +0400

    style: fix style

--- src/components/CardFull/index.tsx
+++ src/components/CardFull/index.tsx
@@ -60,7 +60,7 @@ const CardFull: FC<{
         gap: '12px',
         overflowY: 'auto',
         // overflow: 'hidden',
-        maxHeight: '98vh',
+        maxHeight: '100vh',
       }}
     >
       <CardActionsResume>
