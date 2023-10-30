96038bb85bad6d5acf04db019f1a28c66cab565e
Lvova Alina <lvova.aline@gmail.com>
2023-10-30 16:32:35 +0400

    feat: add cards to page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,31 +3,42 @@ import { SimpleSlide } from '../../components/SimpleSlide';
 import styles from './style.module.scss';
 import SearchBar from '../../ui/search-bar';
 import FilterList from '../../components/FilterList';
-// import CardFull from '../../components/CardFull';
-// import demoResume from '../../assets/data/demoResume';
+import CardSmall from '../../components/CardSmall';
+import demoResume from '../../assets/data/demoResume';
 
 function Main() {
   const [isSlideOpen, setIsSlideOpen] = useState(true);
+
+  const cards = Array(8)
+    .fill(null)
+    .map((_, index) => (
+      <CardSmall
+        key={index}
+        data={demoResume}
+        isViewed={true}
+        isFavourite={true}
+        pdfLink=""
+        onClickLike={() => console.log('test')}
+        onClickDetails={() => console.log('test')}
+        onClickTelegram={() => console.log('test')}
+        onClickEmail={() => console.log('test')}
+        onClickDownload={() => console.log('test')}
+      />
+    ));
 
   return (
     <SimpleSlide isSlideOpen={isSlideOpen} setIsSlideOpen={setIsSlideOpen}>
       <div className={styles.main}>
         <h1 className={styles.main__header}>Поиск кандидатов</h1>
         <div className={styles.main__content}>
-          <SearchBar value="Поиск" />
+          <div className={styles.main__cards}>
+            <SearchBar value="Поиск" />
+            {/* TODO: здесь сделать массив по карточкам */}
+            <div className={styles['main__cards-container']}>{cards}</div>
+          </div>
           <FilterList />
         </div>
       </div>
-      {/* <CardFull
-        data={demoResume}
-        isViewed={true}
-        isFavourite={true}
-        pdfLink="http://ya.ru"
-        onClickLike={() => console.log('test')}
-        onClickTelegram={() => console.log('test')}
-        onClickEmail={() => console.log('test')}
-        onClickDownload={() => console.log('test')}
-      /> */}
     </SimpleSlide>
   );
 }
